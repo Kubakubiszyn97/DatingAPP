a9c730be758dfd4899142e125b39b3a3c96c7b58
Kuba Kubiszyn <kubakubiszyn97@gmail.com>
2019-08-30 13:32:09 +0200

    End of section 10

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -22,8 +22,11 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { UserService } from './_services/user.service';
-import { MemberDetailResolver } from './resolver/member-detail-resolver';
-import { MemberListResolver } from './resolver/member-list-resolver';
+import { MemberDetailResolver } from './_resolvers/member-detail-resolver';
+import { MemberListResolver } from './_resolvers/member-list-resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit-resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 export function tokenGetter() {
@@ -42,7 +45,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -68,7 +72,9 @@ export function tokenGetter() {
       AuthGuard,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -11,15 +11,20 @@ namespace DatingApp.API.Helpers
         {
             CreateMap<User, UserForListDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
-                src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                src.Photos.FirstOrDefault(p => p.IsMain).Url
+                ))
                 .ForMember(dest => dest.Age , opt => opt.MapFrom(src => 
-                src.DateOfBirth.CalculateAge() ));
+                src.DateOfBirth.CalculateAge() 
+                ));
             CreateMap<User, UserForDetailedDto>()
             .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
-                src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                src.Photos.FirstOrDefault(p => p.IsMain).Url
+                ))
             .ForMember(dest => dest.Age , opt => opt.MapFrom(src => 
-                src.DateOfBirth.CalculateAge() ));
+                src.DateOfBirth.CalculateAge() 
+                ));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
